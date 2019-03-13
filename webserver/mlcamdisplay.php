73e1f34531399efae0312be215e0c1a4a3cd59f8
thendawg <npound7110@gmail.com>
2019-03-13 01:12:03 -0500

    Tweaked and removed unneeded elements

--- webserver/mlcamdisplay.php
+++ webserver/mlcamdisplay.php
@@ -5,17 +5,10 @@
 header('Cache-control: no-cache');
 ?>
 <script>
-function httpGet(theUrl)
-{
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open( "GET", theUrl, false );
-    xmlHttp.send( null );
-    return xmlHttp.responseText;
-}
 window.setInterval(function()
 {
-    document.getElementById('mlcam').src = "detectimgs/RD/driveway.jpg?random="+new Date().getTime();
-    document.getElementById('mlcam2').src = "detectimgs/RD/frontyard.jpg?random="+new Date().getTime();
+   document.getElementById('mlcam').src = "detectimgs/RD/driveway.jpg?random="+new Date().getTime();
+   document.getElementById('mlcam2').src = "detectimgs/RD/frontyard.jpg?random="+new Date().getTime();
 }, 475);
 </script>
 <style>
@@ -26,15 +19,11 @@ body {
 </head>
 <body>
 <center>
-	<img id="mlcam" src="detectimgs/RD/drivewaytmp.jpg"
-		onError="this.onerror=null;this.src='detectimgs/RD/driveway.jpg';"
-	>
+	<img id="mlcam" src="detectimgs/RD/drivewaytmp.jpg" \>
 <div style="line-height:40%;">
     <br>
 </div>
-	<img id="mlcam2" src="detectimgs/RD/frontyardtmp.jpg"
-		onError="this.onerror=null;this.src='detectimgs/RD/frontyard.jpg';"
-	>
+	<img id="mlcam2" src="detectimgs/RD/frontyardtmp.jpg" \>
 </center>
 </body>
 </html>
