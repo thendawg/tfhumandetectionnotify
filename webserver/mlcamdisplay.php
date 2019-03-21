797a4a6e7172e159d35c23012481e8b26de0c06a
thendawg <npound7110@gmail.com>
2019-03-21 17:50:03 -0500

    Updated dirs
    Updated to reflect common dir used in py script rather then my custom detectimgs dir

--- webserver/mlcamdisplay.php
+++ webserver/mlcamdisplay.php
@@ -7,8 +7,8 @@ header('Cache-control: no-cache');
 <script>
 window.setInterval(function()
 {
-   document.getElementById('mlcam').src = "detectimgs/RD/driveway.jpg?random="+new Date().getTime();
-   document.getElementById('mlcam2').src = "detectimgs/RD/frontyard.jpg?random="+new Date().getTime();
+   document.getElementById('mlcam').src = "output/RD/driveway.jpg?random="+new Date().getTime();
+   document.getElementById('mlcam2').src = "output/RD/frontyard.jpg?random="+new Date().getTime();
 }, 475);
 </script>
 <style>
@@ -19,20 +19,20 @@ body {
 </head>
 <body>
 <center>
-	<img id="mlcam" src="detectimgs/RD/drivewaytmp.jpg" height="250" width="367"  \>
+	<img id="mlcam" src="output/RD/drivewaytmp.jpg" height="250" width="367"  \>
 <div style="line-height:40%;">
     <br>
 </div>
-	<img id="mlcam2" src="detectimgs/RD/frontyardtmp.jpg" height="250" width="367"  \>
+	<img id="mlcam2" src="output/RD/frontyardtmp.jpg" height="250" width="367"  \>
 </center>
 <script>
 document.getElementById("mlcam").addEventListener("error", ErrorDr);
 function ErrorDr() {
-document.getElementById("mlcam").src = "detectimgs/RD/driveway.jpg";
+document.getElementById("mlcam").src = "output/RD/driveway.jpg";
 }
 document.getElementById("mlcam2").addEventListener("error", ErrorYd);
 function ErrorYd() {
-document.getElementById("mlcam2").src = "detectimgs/RD/frontyard.jpg";
+document.getElementById("mlcam2").src = "output/RD/frontyard.jpg";
 }
 </script>
 </body>
